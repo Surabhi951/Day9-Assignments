0801f740aa7618670b8afde2d9ab92e8bfc623fa
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-17 23:20:30 +0530

    check employee attendance present or absent

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
 public class EmployeeWageComputation {
-    public static void main(String[] args){
+    static final int IS_FULL_TIME = 1;
+
+    static int checkAttendance() {
+        return (int) (Math.random() * 10) % 3;
+    }
+
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int attendance;
+        attendance = checkAttendance();
+        if (attendance == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
