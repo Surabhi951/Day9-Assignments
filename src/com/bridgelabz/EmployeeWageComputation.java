b4439c31753721a51d9a22becf1c879d483d49eb
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-18 00:09:11 +0530

    total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -6,7 +6,8 @@ public class EmployeeWageComputation {
     static final int FULL_DAY_HOUR = 16;
     static final int IS_PART_TIME = 1;
     static final int PART_TIME_HOUR = 8;
-    static final int NO_OF_WORKING_DAYS = 2;
+    static final int MAX_HOURS = 100;
+    static final int MAX_Days = 20;
 
     static int checkAttendance() {
         return (int) (Math.random() * 10) % 3;
@@ -17,26 +18,34 @@ public class EmployeeWageComputation {
         int attendance;
         int dailyWage = 0;
         int totalWage = 0;
-        int day = 0;
+        int day = 1;
+        int totalWorkingHour = 0;
 
-        for (day = 0; day < NO_OF_WORKING_DAYS; day++) {
-            attendance= checkAttendance();
-            switch (attendance) {
+        while (day <= MAX_Days && totalWorkingHour < MAX_HOURS) {
+            attendance = checkAttendance();
+            switch ( attendance) {
                 case IS_FULL_TIME:
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    totalWorkingHour += FULL_DAY_HOUR;
                     break;
 
                 case IS_PART_TIME:
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    totalWorkingHour += PART_TIME_HOUR;
                     break;
                 default:
                     break;
             }
             totalWage += dailyWage;
-            System.out.println("Daily Wage:" + dailyWage);
-            System.out.println("Total Wage of month:" + totalWage);
+            System.out.println("Daily Wage:" + day + " = " + dailyWage);
+            day++;
+
         }
+        System.out.println();
         System.out.println("Total Days:" + (day-1));
+        System.out.println("Total Working Hour:" + totalWorkingHour);
+        System.out.println("Total Wage of month:" + totalWage);
+
     }
 
 }
