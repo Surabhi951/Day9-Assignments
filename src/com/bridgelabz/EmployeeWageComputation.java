2e69d7312d47e594b91ec8aa4d48c328053d7358
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-18 00:29:50 +0530

    compute employee wage for multiple company

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -2,19 +2,17 @@ package com.bridgelabz;
 
 public class EmployeeWageComputation {
     static final int IS_FULL_TIME = 2;
-    static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 16;
     static final int IS_PART_TIME = 1;
     static final int PART_TIME_HOUR = 8;
-    static final int MAX_HOURS = 100;
-    static final int MAX_Days = 20;
 
     static int checkAttendance() {
 
         return (int) (Math.random() * 10) % 3;
     }
 
-    public static int computeEmpWage() {
+    public static int computeEmpWage(String company, int WAGE_PER_HOUR, int MAX_Days, int MAX_HOURS) {
+
         int attendance;
         int dailyWage = 0;
         int totalWage = 0;
@@ -44,12 +42,16 @@ public class EmployeeWageComputation {
         System.out.println();
         System.out.println("Total Days:" + (day-1));
         System.out.println("Total Working Hour:" + totalWorkingHour);
-        System.out.println("Total Wage of month:" + totalWage);
+        System.out.println("Total Wage for company:" + company + " is: " +totalWage);
         return totalWage;
     }
 
- public static void main(String[] args) {
-     System.out.println("Welcome to Employee Wage Computation Program");
-     computeEmpWage();
-  }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        computeEmpWage("Amazon", 20, 2, 10 );
+        computeEmpWage("Meesho", 10, 4, 20 );
+        computeEmpWage("Reliance", 10, 3, 10 );
+        computeEmpWage("Flipcart", 15, 5, 30 );
+
+    }
 }
