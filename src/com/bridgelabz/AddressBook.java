777ec31e22c21654a0c720f056c4672275197a0c
Surabhi951 <surabhibhagat95@gmail.com>
2022-07-18 20:23:30 +0530

    display message

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+        System.out.println("Welcome to the Address Book Program");
     }
 }
